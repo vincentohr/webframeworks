44efce047dd3bfbb80f60493229a348139847976
Vincent <vincent.ohr@hva.nl>
2022-09-15 22:55:54 +0200

    3.2 start (table)

--- wk-frontend-vue1/src/App31.vue
+++ wk-frontend-vue1/src/App31.vue
@@ -1,20 +1,23 @@
 <template>
   <Header msg=""/>
   <NavBar msg=""/>
-  <Welcome msg=""/>
+<!--  <Welcome msg=""/>-->
+  <ScootersOverview31></ScootersOverview31>
 </template>
 
 <script>
 import Header from './components/Header.vue'
 import Welcome from './components/Welcome.vue'
 import NavBar from '@/components/NavBar'
+import ScootersOverview31 from '@/components/scooters/ScootersOverview31'
 
 export default {
   name: 'App31',
   components: {
+    ScootersOverview31,
     NavBar,
-    Header,
-    Welcome
+    Header
+    // Welcome
   }
 }
 </script>
---
+++ wk-frontend-vue1/src/models/scooter.js
@@ -0,0 +1,22 @@
+export class Scooter {
+  constructor (id, tag, status, gpsLocation, mileage, batteryCharge) {
+    this.id = id
+    this.tag = tag
+    this.status = status
+    this.gpsLocation = gpsLocation
+    this.mileage = mileage
+    this.batteryCharge = batteryCharge
+  }
+
+  static createSampleScooter (pId = 0) {
+    const scooter = new Scooter()
+    const statusArray = ['IDLE', 'INUSE', 'MAINTENANCE']
+    const gpsLocations = ['Museumplein', 'Leidseplein', 'Nemo']
+    scooter.gpsLocation = gpsLocations[Math.round(Math.random()) * 3]
+    scooter.tag = Math.random().toString(36).slice(2, 10)
+    scooter.status = statusArray[Math.round(Math.random()) * 3]
+    scooter.mileage = Math.round(Math.random() * 10_000)
+    scooter.batteryCharge = Math.round(Math.random() * 95) + 5
+    return new Scooter(pId, scooter.tag, scooter.status, scooter.gpsLocation, scooter.mileage, scooter.batteryCharge)
+  }
+}
