e23ed866bd6ad5babe79812c21e2961ea7c2635e
Vincent <vincent.ohr@hva.nl>
2022-09-20 20:43:34 +0200

    working on 3.2.2

--- wk-frontend-vue1/src/components/scooters/Detail32.vue
+++ wk-frontend-vue1/src/components/scooters/Detail32.vue
@@ -1,13 +1,81 @@
 <template>
-
+  <div id="content">
+    <table>
+      <tr>
+        <th id="head">Scooter details (id = {{id}} )</th>
+      </tr>
+      <tr>
+        <td>{{tagText}}</td>
+        <td><input type="text" v-model="selectedScooter"></td>
+      </tr>
+      <tr>
+        <td>{{statusText}}</td>
+        <td><select v-model="status">
+          <option selected v-for="state in statusArr" :key="state">{{ state }}</option>
+        </select></td>
+      </tr>
+      <tr>
+        <td>{{batteryChargeText}}</td>
+        <td><input type="text" v-model="batteryCharge"></td>
+      </tr>
+      <tr>
+        <td>{{gpsLocationText}}</td>
+        <td><input type="text" v-model="gpsLocation"></td>
+      </tr>
+      <tr>
+        <td>{{mileageText}}</td>
+        <td><input type="text" v-model="mileage"></td>
+      </tr>
+    </table>
+    <button id="deleteScooter">Delete</button>
+  </div>
 </template>
 
 <script>
+import { Scooter } from '@/models/scooter'
 export default {
-  name: 'Detail32'
+  props: ['selectedScooter', 'status', 'batteryCharge', 'gpsLocation', 'mileage', 'id'],
+  emits: ['delete-scooter'],
+  data () {
+    return {
+      tagText: 'Tag',
+      statusText: 'Status',
+      batteryChargeText: 'Battery Charge (%):',
+      gpsLocationText: 'GPS Location:',
+      mileageText: 'Total Mileage (km):',
+      statusArr: Scooter.statusArray
+    }
+  }
 }
 </script>
 
 <style scoped>
+table {
+  font-family: arial, sans-serif;
+  border-collapse: collapse;
+  width: 30%;
+}
+th{
+  background-color: goldenrod;
+  text-align: center;
+}
+
+td, th {
+  border: 1px solid #dddddd;
+  padding: 8px;
+}
+
+tr:nth-child(even) {
+  background-color: grey;
+}
+
+input{
+  width: 95%;
+}
+#deleteScooter{
+  display: block;
+  margin-left: 25%;
+  margin-right: auto;
+}
 
 </style>
--- wk-frontend-vue1/src/components/scooters/Overview32.vue
+++ wk-frontend-vue1/src/components/scooters/Overview32.vue
@@ -6,51 +6,91 @@
           Tag:
         </th>
       </tr>
-      <tr v-for="(tag, index) in tags" @click.left="selected (index)" :key="index">
-        <td>{{tag}}</td>
+      <tr v-for="(tag, index) in tags" :key="index" @click="selected (index)">
+        <td :class="activeTagClass" @click="selectTag">{{ tag }}</td>
       </tr>
-      <tr>{{selectedScooter}}</tr>
     </table>
-    <button @click="onNewScooter">New Scooter</button>
+    <button @click="newTag">New Scooter</button>
   </div>
+  <p>{{ text }}</p>
+  <Detail32
+    v-model:selected-scooter="selectedScooter"
+    v-model:status="status"
+    v-model:battery-charge="batteryCharge"
+    v-model:gps-location="gpsLocation"
+    v-model:mileage="mileage"
+    v-model:id="id"
+  >
+  </Detail32>
 </template>
 
 <script>
+import Detail32 from '@/components/scooters/Detail32'
 import { Scooter } from '@/models/scooter'
 
 export default {
-  props: ['scooters'],
+  components: { Detail32 },
   data () {
     return {
-      count: 0,
+      id: '',
+      number: '',
+      count: '',
+      text: 'Select a scooter',
       selectedScooter: null,
-      tags: ['td1', 'td2', 'td3', 'td4', 'td5', 'td6']
+      scooter: '',
+      tags: [],
+      status: '',
+      batteryCharge: '',
+      gpsLocation: '',
+      mileage: '',
+      isActive: false
     }
   },
   methods: {
     selected (index) {
+      // this.count++
+      // this.text = ''
+      // if (index === this.number && this.count >= 2) {
+      //   this.selectedScooter = null
+      //   this.count = 0
+      // }
       this.selectedScooter = this.tags.at(index)
-      this.count++
-      const name = this.selectedScooter
+      this.number = index
+      this.status = Scooter.createSampleScooter(index).status
+      this.batteryCharge = Scooter.createSampleScooter(index).batteryCharge
+      this.gpsLocation = Scooter.createSampleScooter(index).gpsLocation
+      this.mileage = Scooter.createSampleScooter(index).mileage
+      this.id = Scooter.createSampleScooter(index).id
     },
-    onNewScooter (pId) {
+    newTag (pId) {
       pId = this.lastId
       this.lastId++
-      this.scooters.push(
-        Scooter.createSampleScooter(pId)
+      this.tags.push(
+        Scooter.createSampleScooter(pId).tag
       )
+    },
+    selectTag () {
+      this.isActive = !this.isActive
+    }
+  },
+  computed: {
+    activeTagClass () {
+      return { activeTag: this.isActive }
     }
   }
 }
 </script>
 
 <style scoped>
+
 table {
+  margin-top: 3%;
   font-family: arial, sans-serif;
-    border-collapse: collapse;
-    width: 20%;
+  border-collapse: collapse;
+  width: 30%;
 }
-th{
+
+th {
   background-color: goldenrod;
   text-align: center;
 }
@@ -61,6 +101,10 @@ td, th {
 }
 
 tr:nth-child(even) {
-  background-color: #dddddd;
+  background-color: grey;
+}
+
+.activeTag {
+  background-color: goldenrod;
 }
 </style>
