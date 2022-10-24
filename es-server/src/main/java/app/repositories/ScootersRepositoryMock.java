1fa5310945963bc8f1aeecd125f07d0b7ba1927a
joliw <joliweibolt@gmail.com>
2022-10-24 18:43:24 +0200

    test

--- es-server/src/main/java/app/repositories/ScootersRepositoryMock.java
+++ es-server/src/main/java/app/repositories/ScootersRepositoryMock.java
@@ -6,7 +6,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 @Component
